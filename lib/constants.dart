bb224277d968440c79bb9b0c23094d2aba7d5b3d
codeme5 <ma08056@st.habib.edu.pk>
2024-06-25 14:48:18 +0500

    added consts and some beautifications to the code

--- lib/constants.dart
+++ lib/constants.dart
@@ -17,3 +17,22 @@ const kMessageContainerDecoration = BoxDecoration(
     top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
   ),
 );
+
+InputDecoration getInputDecor(String hint) {
+  return InputDecoration(
+    hintText: hint,
+    contentPadding:
+        const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    border: const OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+    enabledBorder: const OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+    focusedBorder: const OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+  );
+}
